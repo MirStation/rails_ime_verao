c96678ead09b27ab7a43cece19c385771ebb7e0e
António Martins Miranda <amartmiranda@gmail.com>
2017-01-27 21:49:21 -0200

    Implementando associações entre atores, filmes e diretores.

--- my_movies/db/schema.rb
+++ my_movies/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170127213458) do
+ActiveRecord::Schema.define(version: 20170127234020) do
 
   create_table "actors", force: :cascade do |t|
     t.string   "name"
@@ -20,12 +20,18 @@ ActiveRecord::Schema.define(version: 20170127213458) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "actors_movies", id: false, force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "actor_id"
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "release_date"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "director_id"
   end
 
   create_table "professionals", force: :cascade do |t|
